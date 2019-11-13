d9b5de57eb27fcdc8327873e8e19b25830233122
lixiaoyu <lixiaoyu@cont.com.cn>
2019-11-13 09:28:38 +0800

    1

--- php/client/live2d/addLive2d.php
+++ php/client/live2d/addLive2d.php
@@ -4,9 +4,13 @@ include "../common/index.php";
 $content = $_GET['content'];
 $ip = $_GET['ip'];
 $username = $_GET['username'];
-
+if(empty($content)){
+  $returnResult = new Fail();
+  $returnResult->data = "新增失败";
+  echo json_encode($returnResult);
+  return;
+}
 $sql = "INSERT INTO LIVE2D (content,ip,author) VALUES ('$content','$ip','$username')";
-
 $result = $conn->query($sql);
 if($result){
     $returnResult = new Success();
